66287c50388a669e9880dbccd640d03cc28ac39d
iSabyasachi <s.sabyasachi@outlook.com>
2022-07-08 23:24:42 -0400

    updated

--- src/main/java/com/spring/security/config/ProjectSecurityConfig.java
+++ src/main/java/com/spring/security/config/ProjectSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -82,8 +83,13 @@ public class ProjectSecurityConfig {
      *
      * @return
      */
+	/*
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+    }*/
+	@Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
